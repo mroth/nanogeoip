fb7345ad34770230e8b7297b16ae13e168af22c0
Matthew Rothenberg <mrothenberg@gmail.com>
2018-12-27 23:03:14 -0500

    trivial: cleanup inactive db benches
    Not using these for now, but give it a reasonable name and set it to use the testdata directory to avoid confusion since it's still sitting around in source code.

--- benches/db_benchmarks.rs
+++ benches/db_benchmarks.rs
@@ -6,10 +6,15 @@ use criterion::Criterion;
 use std::net::IpAddr;
 use std::str::FromStr;
 
+const TESTDATA: &'static str = "testdata/GeoIP2-City-Test.mmdb";
+
+// These benches don't do too much, but just verify that our minimal lookup
+// function should be somewhat faster than a default maxminddb lookup for the
+// entire geoip2::City struct which contains a bunch of fields.
 fn criterion_benchmark(c: &mut Criterion) {
-    let db = nanogeoip::Reader::open("data/GeoLite2-City.mmdb").unwrap();
+    let db = nanogeoip::Reader::open(TESTDATA).unwrap();
+    let mmdb = maxminddb::Reader::open_readfile(TESTDATA).unwrap();
     let ip: IpAddr = FromStr::from_str("89.160.20.112").unwrap();
-    let mmdb = maxminddb::Reader::open_readfile("data/GeoLite2-City.mmdb").unwrap();
 
     c.bench_function("lookup", move |b| b.iter(|| db.lookup(ip)));
     c.bench_function("mmlook", move |b| {
